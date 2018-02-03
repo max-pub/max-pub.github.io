42a691b76ff94a9c11b28cfd88c752071803bb20
Max Fechner <max.fechner@gmail.com>
2018-02-02 21:19:17 -0600

    weather

---
+++ w/date-format.tag.js
@@ -0,0 +1,15 @@
+window.customElements.define('date-format', class extends HTMLElement {
+	static get observedAttributes() {
+		return ['date', 'format'];
+	}
+	attributeChangedCallback(attr, oldVal, newVal) {
+		console.log(attr, newVal);
+		this[attr] = newVal;
+		this.update();
+	}
+	update() {
+		console.log(this.date, new Date(Date.parse(this.date)));
+		if (this.date && this.format)
+			this.innerText = new Date(Date.parse(this.date)).format(this.format);
+	}
+});
---
+++ w/weather-chart.tag.js
@@ -0,0 +1,42 @@
+document.head.insertAdjacentHTML('beforeend', `<template id="weather-chart"><style>
+	:host{display: block; position: relative; }
+	line-chart{position:absolute; width:100%;}
+	#clouds{ fill: #aaa; opacity: 1.0;}
+	#rain{ fill: cornflowerBlue; opacity: 1;}
+	#wind{ fill: transparent; stroke: #ccc; stroke-width: 5; stroke-dasharray: 10,10; stroke-linecap: round;}
+	#temp{ stroke: #BD4900; stroke-width:10; opacity: 1; fill: transparent;}
+</style>
+	<line-chart id="clouds" aspect-ratio="3" y-min="1" y-max="100" smooth="" fill=""></line-chart>
+	<line-chart id="rain" aspect-ratio="3" y-min="0.01" y-max="100" smooth="" fill="" inverse=""></line-chart>
+	<line-chart id="wind" aspect-ratio="3" y-min="1" y-max="30" smooth=""></line-chart>
+	<line-chart id="temp" aspect-ratio="3" y-min-pad="1" y-max-pad="1" smooth=""></line-chart>
+</template>`);
+window.customElements.define('weather-chart', class extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({mode: 'open'}).appendChild(document.querySelector('#weather-chart').content.cloneNode(true));
+	}
+ 	
+	setData(data){
+		this.data = {temp:[],clouds:[],rain:[],wind:[]};
+		for(var i=0; i<data.length; i++){
+			let hour = data[i];
+			// if(!hour) continue;
+			this.data.temp.push(hour ? hour.temperature.real : 0);
+			this.data.rain.push(hour ? hour.precipitation.probability*100 : 0);
+			this.data.wind.push(hour ? hour.wind.speed*3.6 : 0);
+			this.data.clouds.push(hour ? hour.cloudCover*100 : 0)
+		}
+		this.setChart('temp');
+		this.setChart('clouds');
+		this.setChart('rain');
+		this.setChart('wind');
+	}
+
+	setChart(typ) {
+		// console.log(typ,this.data[typ],this.shadowRoot.querySelector('#temp'));
+		this.shadowRoot.querySelector('#'+typ).setAttribute('values',JSON.stringify(this.data[typ])); 
+	}
+
+
+});
